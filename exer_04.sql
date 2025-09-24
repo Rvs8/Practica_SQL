fd120135aed315d74b86d99dab2ae9712e7f937d
Roger Verges <roni.vs.1992@gmail.com>
2025-09-24 12:18:23 +0200

    exer_04.sql
    Generar el campo vdn_aggregation

---
+++ exer_04.sql
@@ -0,0 +1,10 @@
+CREATE TABLE keepcoding.ivr_summary_vdn AS
+SELECT
+  ivr_id AS calls_ivr_id,
+  CASE
+    WHEN STARTS_WITH(vdn_label, 'ATC') THEN 'FRONT'
+    WHEN STARTS_WITH(vdn_label, 'TECH') THEN 'TECH'
+    WHEN vdn_label = 'ABSORTION' THEN 'ABSORTION'
+    ELSE 'RESTO'
+  END AS vdn_aggregation
+FROM keepcoding.ivr_calls;
