f46867d5efc7b59d1e1fb23f7c95c9d37d08360d
Roger Verges <roni.vs.1992@gmail.com>
2025-09-24 12:39:37 +0200

    exer_05.sql
    Generar los campos document_type y document_identification

---
+++ exer_05.sql
@@ -0,0 +1,21 @@
+SELECT
+  ivr_id AS calls_ivr_id,
+
+  -- Seleccionamos un solo cliente por llamada
+  (
+    SELECT document_type
+    FROM keepcoding.ivr_steps AS st
+    WHERE st.ivr_id = ca.ivr_id
+      AND document_type IS NOT NULL
+    LIMIT 1
+  ) AS document_type,
+
+  (
+    SELECT document_identification
+    FROM keepcoding.ivr_steps AS st
+    WHERE st.ivr_id = ca.ivr_id
+      AND document_identification IS NOT NULL
+    LIMIT 1
+  ) AS document_identification
+
+FROM keepcoding.ivr_calls AS ca;
