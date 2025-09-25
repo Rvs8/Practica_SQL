f25eb4679574df5e61e4f65ddf0bc40819d8887c
Roger Verges <roni.vs.1992@gmail.com>
2025-09-25 09:14:17 +0200

    exer_06.sql
    Generar campo customer_phone

--- exer_06.sql
+++ exer_06.sql
@@ -1,13 +1,14 @@
 SELECT
-  ivr_id AS calls_ivr_id,
+  ca.ivr_id AS calls_ivr_id,
 
-  -- Seleccionamos un solo número de teléfono por llamada
   (
     SELECT customer_phone
     FROM keepcoding.ivr_steps AS st
     WHERE st.ivr_id = ca.ivr_id
       AND customer_phone IS NOT NULL
+      AND customer_phone != 'UNKNOWN'
     LIMIT 1
   ) AS cust_phone
 
 FROM keepcoding.ivr_calls AS ca;
+
