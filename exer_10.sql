6b1515b25ed3e7fbb4457cebd3756b67a287e303
Roger Verges <roni.vs.1992@gmail.com>
2025-09-25 09:41:30 +0200

    exer_10.sql
    Generar campo info_by_dni_lg

---
+++ exer_10.sql
@@ -0,0 +1,8 @@
+SELECT 
+  det.ivr_id,
+  CASE 
+    WHEN COUNTIF(step_name = 'CUSTOMERINFOBYDNI.TX' AND step_result = 'OK') > 0 THEN 1
+    ELSE 0
+  END AS info_by_dni_lg
+FROM keepcoding.ivr_detail AS det
+GROUP BY det.ivr_id;
