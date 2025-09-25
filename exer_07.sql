86347f77cb2999e1bbea6b8d196b7cac7d141953
Roger Verges <roni.vs.1992@gmail.com>
2025-09-25 09:11:30 +0200

    exer_07.sql
    Generar campo billing_account_id

--- exer_07.sql
+++ exer_07.sql
@@ -1,5 +1,5 @@
 SELECT
-  ivr_id AS calls_ivr_id,
+  ca.ivr_id AS calls_ivr_id,
 
   -- Seleccionamos un solo número de cliente por llamada
   (
@@ -7,7 +7,9 @@ SELECT
     FROM keepcoding.ivr_steps AS st
     WHERE st.ivr_id = ca.ivr_id
       AND billing_account_id IS NOT NULL
+      AND billing_account_id != 'UNKNOWN'
     LIMIT 1
   ) AS bill_ac_id
 
 FROM keepcoding.ivr_calls AS ca;
+
